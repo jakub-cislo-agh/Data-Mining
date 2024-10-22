1959f336886c0c471fd5c7b9ec2b2aee4102c23d
jakub-cislo-agh <jakubcislo@student.agh.edu.pl>
2024-10-22 11:52:19 +0200

    komentarze pietron

--- dbscan.py
+++ dbscan.py
@@ -65,3 +65,10 @@ if __name__ == "__main__":
     plt.scatter(X[:, 0], X[:, 1], c=dbscan.labels_)
     plt.title("Klasteryzacja DBSCAN")
     plt.show()
+
+#porównać z dbscanem z biblioteki
+#unit testy
+#mnist – zbiórt danych
+#porównać z K-means (scikit learn)
+#podzielić kod: test, główna klasa
+#github - manual, wizualizacja
