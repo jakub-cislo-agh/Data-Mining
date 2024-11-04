7b3880c3954f2d462d7e362f2928ed8fea2e37c6
jakub-cislo-agh <jakubcislo@student.agh.edu.pl>
2024-11-04 22:40:48 +0100

    k-means comparison and minor adjustments

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-if __name__ == "__main__":
-    from MySCAN import *
-    from sklearn.datasets import make_blobs
-    import matplotlib.pyplot as plt
+from MySCAN import *
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
 
-    # przykładowy zbiór testowy z sklearn.dataset
-    X, _ = make_blobs(n_samples=100, centers=3, random_state=37)
+# przykładowy zbiór testowy z sklearn.dataset
+X, _ = make_blobs(n_samples=100, centers=3, random_state=37)
 
-    # tutaj sobie klasteryzujemy
-    dbscan = MySCAN(eps=1, min_pts=5) # tutaj podajemy parametry eps i min_pts
-    dbscan.clusterize(X)
+# tutaj sobie klasteryzujemy
+dbscan = MySCAN(eps=1, min_pts=5) # tutaj podajemy parametry eps i min_pts
+dbscan.clusterize(X)
 
-    # wizualizacja klastrów
-    plt.scatter(X[:, 0], X[:, 1], c=dbscan.labels_)
-    plt.title("Klasteryzacja DBSCAN")
-    plt.show()
+# wizualizacja klastrów
+plt.scatter(X[:, 0], X[:, 1], c=dbscan.labels_)
+plt.title("Klasteryzacja DBSCAN")
+plt.show()
 
 #porównać z dbscanem z biblioteki
 #unit testy
